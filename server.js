e37bb0752c55f73230c9cc3c3528415be1691b79
Jonathan Hanahan <jonathan.hanahan@gmail.com>
2024-04-03 13:10:31 -0500

    views folder and server update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,10 +17,9 @@ const sess = {
   
   app.use(session(sess));
 
-// const hbs = exphbs.create({ helpers });
-
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
